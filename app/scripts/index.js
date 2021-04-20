4ea2a0ff03f1e40de1aa6631e29c8bde75a79d1d
Polyakova Maria <polyakovamy@yandex.ru>
2021-04-20 22:18:11 +0300

    Создание dropdown-menu

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,42 @@
 import '../styles/main.scss'
 import Inputmask from 'inputmask'
 
+// _input-masked
 if (process.env.NODE_ENV !== 'production') {
   require('../index.pug')
 };
 Inputmask('99.99.9999').mask(document.querySelector('.input-masked-date'))
+
+// dropdown-menu
+let dropdownSelected = document.querySelector('.dropdown-menu__selectedItem')
+let dropdownList = document.querySelector('.dropdown-menu__list')
+
+function addShow () {
+  dropdownList.classList.toggle('show')
+};
+
+dropdownSelected.addEventListener('click', addShow)
+
+// dropdown-menu counter
+const counter = function () {
+  const btns = document.querySelectorAll('.dropdown-menu__counter-btn')
+  btns.forEach(btn => {
+    btn.addEventListener('click', function () {
+      const direction = this.dataset.direction
+      const inp = this.parentElement.querySelector('.dropdown-menu__count')
+      const btnMinus = this.parentElement.querySelector('.dropdown-menu__buttonMinus')
+      const currentValue = +inp.textContent
+      let newValue
+      if (direction === 'plus') {
+        newValue = currentValue + 1
+      } else {
+        newValue = currentValue - 1 > 0 ? currentValue - 1 : 0
+      }
+      inp.textContent = newValue
+      if (newValue === 0) {
+        btnMinus.classList.add('opacity_minus')
+      } else btnMinus.classList.remove('opacity_minus')
+    })
+  })
+}
+counter()
